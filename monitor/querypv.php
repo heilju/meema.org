9045370424cb7848098dc97dff0e3e1021e93fee
Heil Juergen <juergen.heil@meema.org>
2014-06-28 22:30:08 +0200

    added state and if section for state "off"

--- monitor/querypv.php
+++ monitor/querypv.php
@@ -7,52 +7,78 @@ $password = "eTcJ0708=";
 
 $outputFile = "pvdata.csv";
 
-// XPath current power output
-$xpathCurrentOutput = "/html/body/form/font/table[2]/tbody/tr[4]/td[3]";
+// XPath current power output (state: on)
+$xpathCurrentOutput = "/html/body/form/font/table[2]/tbody/tr[4]/td[3]"; //during state on
+//$xpathCurrentOutput = "/html/body/form/font/table[2]/tr[4]/td[3]"; //during state off
 
-// XPatch total power output
+// XPath total power output
 $xpathTotalOutput = "/html/body/form/font/table[2]/tbody/tr[4]/td[6]";
 
+// XPath PV state
+$xPathPvState = "/html/body/form/font/table[2]/tr[8]/td[3]";
 
-/*
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+curl_setopt($ch, CURLOPT_VERBOSE, true);
+
 $html = curl_exec($ch);
+
 curl_close($ch);
-*/
-# Create a DOM parser object
+
+//echo $html;
+
+// Create a DOM parser object
 $dom = new DOMDocument();
 
-# Parse the HTML from PV server.
-# The @ before the method call suppresses any warnings that
-# loadHTML might throw because of invalid HTML in the page.
-//@$dom->loadHTML($html);
-@$dom->loadHTMLFile("pvserver.html");
+/*
+Parse the HTML from PV server.
+The @ before the method call suppresses any warnings that
+loadHTML might throw because of invalid HTML in the page.
+*/
+@$dom->loadHTML($html);
+//@$dom->loadHTMLFile("pvserver.html");
 
 $xpath = new DOMXpath($dom);
 
 $handle = fopen($outputFile, "a");
 
-// get current power output
-$elements = $xpath->query($xpathCurrentOutput);
+// get pv state
+$elements = $xpath->query($xPathPvState);
 
 foreach ($elements as $element) {
-    fwrite($handle, date("Y-m-d H:i:s") . ";" . trim($element->nodeValue). "\r\n");
+    $pvState = trim($element->nodeValue);
+}
+
+if ($pvState =! "Aus")
+{
+    // get current power output
+    $elements = $xpath->query($xpathCurrentOutput);
+    echo $elements->length;
+
+    foreach ($elements as $element) {
+        fwrite($handle, date("Y-m-d H:i:s") . ";" . trim($element->nodeValue). "\r\n");
+    }
+}
+else
+{
+    // get current power output
+    fwrite($handle, date("Y-m-d H:i:s") . ";0\r\n");
+
 }
 
 fclose($handle);
 
-// loop through all <td> tags and print XPath, Value and Node Type
 /*
+// loop through all <td> tags and print XPath, Value and Node Type
+
 $tds = $dom->getElementsByTagName('td');
 
 foreach ($tds as $td) {
     echo "Node Value:" . $td->nodeValue . ", Node Type:" . $td->nodeType . ", Node XPath:" . $td->getNodePath() . "<br \>";
 }
 */
-
 ?>
